3c3e733caa1dbbb201f9201d1933c2e354444fe1
Fsp30 <depaulacostafilipe@gmail.com>
2025-09-23 20:42:50 -0300

    read environments

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/Fsp30/Pokedex_with_golang/handlers"
 )
 
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+
 	r := gin.Default()
 
 	r.GET("/pokemon/:name", handlers.GetPokemonInfo)
